1d045370af8a2ec5e162048f8e5bb7fd0464e96c
RyoDev <ytryo789@gmail.com>
2021-09-01 00:22:42 +0700

    Update

---
+++ src/AfaraRestApi/AfaraMyId.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ryodevz\AfaraRestApi;
+
+use Ryodevz\HttpfulWrapper\Http;
+
+class AfaraMyId
+{
+    public static $base = 'https://public-restapi.herokuapp.com/api/';
+
+    public static function json($link)
+    {
+        return Http::get(self::$base . $link)->send()->body;
+    }
+
+    public static function TextOrPlain($link)
+    {
+        return Http::get(self::$base . $link)->send()->body;
+    }
+}
---
+++ src/AfaraRestApi/Joox.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Ryodevz\AfaraRestApi;
+
+use Ryodevz\AfaraRestApi\AfaraMyId;
+
+class Joox
+{
+    private $base = 'joox';
+
+    private $search = '/search';
+
+    private $album = '/album';
+
+    private $show = '/show';
+
+    public function search(string $q)
+    {
+        return AfaraMyId::json($this->base . $this->search . '?q=' . $q);
+    }
+
+    public function album(string $album_id)
+    {
+        return AfaraMyId::json($this->base . $this->album . '?id=' . $album_id);
+    }
+
+    public function show(string $id)
+    {
+        return AfaraMyId::json($this->base . $this->show . '?id=' . $id);
+    }
+}
