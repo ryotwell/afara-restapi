310fce9c69c721fdcbe2b039f956d5c935ff4565
RyoDev <ytryo789@gmail.com>
2021-09-01 00:15:21 +0700

    Fix bug

--- src/AfaraMyId.php
+++ src/AfaraMyId.php
@@ -6,7 +6,7 @@ use Ryodevz\HttpfulWrapper\Http;
 
 class AfaraMyId
 {
-    public static $base = 'https://public-restapi.herokuapp.com/';
+    public static $base = 'https://public-restapi.herokuapp.com/api/';
 
     public static function json($link)
     {
